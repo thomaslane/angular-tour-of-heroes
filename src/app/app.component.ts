97ba987e8f694d0d194cce582a6eb9ca13784140
Thomas Lane <thomas@mytimeslot.co.za>
2017-06-30 00:34:06 +0200

    End of day 2, half way through part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,22 @@ export class Hero {
     name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+]
+
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 
@@ -25,4 +30,5 @@ export class AppComponent {
       id: 1,
       name: 'Windstorm'
   };
+  heroes = HEROES;
 }
